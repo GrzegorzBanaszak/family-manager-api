6f82190d4aa5a8fae85af5f367d9e605b2161e1b
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-29 22:54:17 +0200

    Initial project

---
+++ dist/server.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
