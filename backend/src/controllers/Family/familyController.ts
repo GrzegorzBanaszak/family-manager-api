c4b075c36e321d8579705501df9bdbd1b32d9d60
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-30 21:13:42 +0200

    Get the transactions with a family

--- backend/src/controllers/Family/familyController.ts
+++ backend/src/controllers/Family/familyController.ts
@@ -2,10 +2,9 @@ import asyncHandelr from "express-async-handler";
 import Family from "../../models/familyModel";
 
 const getFamily = asyncHandelr(async (req, res) => {
-  const family = await Family.findById(req.user.memberOfFamily).populate(
-    "familyMembers",
-    "_id firstName lastName email"
-  );
+  const family = await Family.findById(req.user.memberOfFamily)
+    .populate("familyMembers", "_id firstName lastName email")
+    .populate("transactions", "_id user amount transactionType");
 
   if (!family) {
     res.status(400);
